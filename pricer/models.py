572ff91dd25ef7cecec7287cf25561e7a02eb44a
Dante Anthony <calikidd86@gmail.com>
2019-05-05 19:32:24 -0700

    Added Character Model and updated Pop Model with ForeignKey to Character.

--- pricer/models.py
+++ pricer/models.py
@@ -7,7 +7,15 @@ BASE_URL = "https://www.poppriceguide.com/guide/searchresults.php?search="
 
 
 # Create your models here.
+class Character(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Pop(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, default=None)
     name = models.CharField(max_length=200)
     value = models.DecimalField(max_digits=10, decimal_places=2)
     url = models.CharField(max_length=200)
